37a3cc522b4a4d7f99930bb96ca5c5f5dc18a47a
Josh Zuech <josh.zuech@nextthought.com>
2018-03-01 14:25:38 -0600

    Add DefaultRequired policy adapter

--- src/nti/contenttypes/completion/policies.py
+++ src/nti/contenttypes/completion/policies.py
@@ -8,13 +8,10 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
-from BTrees.OOBTree import OOTreeSet
-
 from zope import interface
 
 from zope.container.contained import Contained
 
-from nti.contenttypes.completion.interfaces import ICompletableItemDefaultRequiredPolicy
 from nti.contenttypes.completion.interfaces import ICompletableItemAggregateCompletionPolicy
 
 from nti.dublincore.time_mixins import PersistentCreatedAndModifiedTimeObject
@@ -58,15 +55,3 @@ class CompletableItemAggregateCompletionPolicy(PersistentCreatedAndModifiedTimeO
                 result = False
         return result
 
-
-@interface.implementer(ICompletableItemDefaultRequiredPolicy)
-class CompletableItemDefaultRequiredPolicy(PersistentCreatedAndModifiedTimeObject,
-                                           SchemaConfigured):
-
-    createDirectFieldProperties(ICompletableItemDefaultRequiredPolicy)
-
-    mimeType = mime_type = "application/vnd.nextthought.completion.defaultrequiredpolicy"
-
-    def __init__(self, *args, **kwargs):
-        super(CompletableItemDefaultRequiredPolicy, self).__init__(*args, **kwargs)
-        self.mime_types = OOTreeSet()
