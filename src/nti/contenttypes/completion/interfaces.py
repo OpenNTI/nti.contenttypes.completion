6337688adccfc9a87fa51ed02f4a5a41020d257c
Sheldon Smith <sheldon.smith@nextthought.com>
2018-03-28 17:38:12 -0400

    Add ICourseCompletionProgress multi adapter and factory

--- src/nti/contenttypes/completion/interfaces.py
+++ src/nti/contenttypes/completion/interfaces.py
@@ -387,3 +387,9 @@ class UserProgressUpdatedEvent(AbstractUserProgressEvent):
 @interface.implementer(IUserProgressRemovedEvent)
 class UserProgressRemovedEvent(AbstractUserProgressEvent):
     pass
+
+
+class ICourseCompletionProgress(interface.Interface):
+    """
+    Determines the completion progress for a user in a course
+    """
