a37248a9f5751b5b8bd9ec20bdb58fb1bc498fe2
Josh Zuech <josh.zuech@nextthought.com>
2018-04-02 11:46:00 -0500

    PolicyContainer and RequiredItemContainer externalization/internalization

--- src/nti/contenttypes/completion/externalization.py
+++ src/nti/contenttypes/completion/externalization.py
@@ -13,12 +13,17 @@ from __future__ import absolute_import
 from zope import component
 from zope import interface
 
+from nti.contenttypes.completion.interfaces import ICompletableItemContainer
 from nti.contenttypes.completion.interfaces import ICompletableItemDefaultRequiredPolicy
+from nti.contenttypes.completion.interfaces import ICompletionContextCompletionPolicyContainer
 
 from nti.externalization.externalization import to_external_object
 from nti.externalization.externalization import to_standard_external_dictionary
 
 from nti.externalization.interfaces import IExternalObject
+from nti.externalization.interfaces import StandardExternalFields
+
+ITEMS = StandardExternalFields.ITEMS
 
 
 @component.adapter(ICompletableItemDefaultRequiredPolicy)
@@ -31,4 +36,33 @@ class _CompletableItemDefaultRequiredPolicyExternalObject(object):
     def toExternalObject(self, **kwargs):
         result = to_standard_external_dictionary(self.policy, **kwargs)
         result['mime_types'] = [to_external_object(x) for x in self.policy.mime_types]
+        return result
+
+
+@component.adapter(ICompletableItemContainer)
+@interface.implementer(IExternalObject)
+class _CompletableItemContainerExternalObject(object):
+
+    def __init__(self, container):
+        self.container = container
+
+    def toExternalObject(self, **kwargs):
+        result = to_standard_external_dictionary(self.container, **kwargs)
+        result['required'] = list(self.container.get_required_keys())
+        result['optional'] = list(self.container.get_optional_keys())
+        return result
+
+
+@component.adapter(ICompletionContextCompletionPolicyContainer)
+@interface.implementer(IExternalObject)
+class _CompletionContextCompletionPolicyContainerExternalObject(object):
+
+    def __init__(self, container):
+        self.container = container
+
+    def toExternalObject(self, **kwargs):
+        result = to_standard_external_dictionary(self.container, **kwargs)
+        # TODO: Why do I have to do this?
+        result['context_policy'] = to_external_object(self.container.context_policy)
+        result[ITEMS] = {key:to_external_object(val) for key, val in self.container.items()}
         return result
